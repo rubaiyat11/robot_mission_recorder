78d207858a0217c45027a94979948acc16dcb77c
rubaiyat11 <rubaiyatabdullahrabi71@gmail.com>
2026-04-12 18:24:56 +0600

    Removed build and log C boilerplate code

--- ros2_ws/src/robot_mission_recorder/install/robot_interface/local/lib/python3.10/dist-packages/robot_interface/action/__init__.py
+++
@@ -1,1 +0,0 @@
-from robot_interface.action._execute_mission import ExecuteMission  # noqa: F401
--- ros2_ws/src/robot_mission_recorder/install/robot_interface/share/robot_interface/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(robot_interface_IDL_FILES "action/ExecuteMission.idl")
-set(robot_interface_INTERFACE_FILES "action/ExecuteMission.action")
