d64aafa4e5a75d1f42e59eb4850cfbdf98be79a2
rubaiyat11 <rubaiyatabdullahrabi71@gmail.com>
2026-04-12 18:06:53 +0600

    Complete robot mission recorder project

--- ros2_ws/src/robot_mission_recorder/build/robot_interface/robot_interface__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nemo/ros2_ws/src/robot_mission_recorder/build/robot_interface/robot_interface__py
-# Build directory: /home/nemo/ros2_ws/src/robot_mission_recorder/build/robot_interface/robot_interface__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
